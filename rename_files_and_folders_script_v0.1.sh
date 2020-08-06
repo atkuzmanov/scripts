aa0420ffe61b8a72c27b3177a61e530a663a3ba0
atkuzmanov <atkuzmanov@gmail.com>
2020-08-06 13:18:38 +0300

    Work-In-Progress-WIP-2020-08-06-131838

--- rename_files_and_folders_script_v0.1.sh
+++ rename_files_and_folders_script_v0.1.sh
@@ -4,10 +4,37 @@ set -e
 
 ################################
 
+declare -a STRINGS_TO_REPLACE
+# STRINGS_TO_REPLACE+=("$n")
+
+
+    for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+    do
+    	echo $current_string
+    done
 
 
 ################################
-################################
+
+## https://unix.stackexchange.com/questions/139363/recursively-iterate-through-files-in-a-directory
+## https://askubuntu.com/questions/266179/how-to-exclude-ignore-hidden-files-and-directories-in-a-wildcard-embedded-find
+
+while IFS=  read -r -d '' n; do
+    
+    # test -f "$n" || echo "$n is not a file."
+    # test -d "$n" || echo "$n is not a directory."
+    echo "$n"
+done < <(find . \( ! -regex '.*/\..*' \) -type f -print0)
+    
+
 ################################
 
+################################
+    
+    ## maybe use this if recursive is not needed
+    ## or use find and add mindepth and maxdepth
+    # for f in * ;
+    # do
+    # done
 
+################################
