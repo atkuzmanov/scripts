b5f208c7645e999c6beac542ae4bd41e717142f5
atkuzmanov <atkuzmanov@gmail.com>
2020-08-06 13:45:00 +0300

    Work-In-Progress-WIP-2020-08-06-134500

--- rename_files_and_folders_script_v0.1.sh
+++ rename_files_and_folders_script_v0.1.sh
@@ -5,27 +5,40 @@ set -e
 ################################
 
 declare -a STRINGS_TO_REPLACE
+STRINGS_TO_REPLACE=("alk8915" "alk198915")
 # STRINGS_TO_REPLACE+=("$n")
-
-
-    for current_string in "${STRINGS_TO_REPLACE[@]}" ;
-    do
-    	echo $current_string
-    done
-
+# for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+# do
+#     echo $current_string
+# done
 
 ################################
 
 ## https://unix.stackexchange.com/questions/139363/recursively-iterate-through-files-in-a-directory
 ## https://askubuntu.com/questions/266179/how-to-exclude-ignore-hidden-files-and-directories-in-a-wildcard-embedded-find
+## https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash
 
-while IFS=  read -r -d '' n; do
-    
-    # test -f "$n" || echo "$n is not a file."
-    # test -d "$n" || echo "$n is not a directory."
-    echo "$n"
-done < <(find . \( ! -regex '.*/\..*' \) -type f -print0)
-    
+rename_files_and_folders() {
+    while IFS=  read -r -d '' n; do
+
+        if [[ "$n" == *"alk19890105"* ]]; 
+        then
+            echo "Skipping $n"
+        else
+            for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+            do
+                if [[ "$n" == *"$current_string"* ]]; 
+                then
+                    echo $n
+                fi
+            done
+        fi
+
+        # echo "$n"
+    done < <(find . \( ! -regex '.*/\..*' \) -type f -print0)
+}
+
+rename_files_and_folders
 
 ################################
 
@@ -35,6 +48,8 @@ done < <(find . \( ! -regex '.*/\..*' \) -type f -print0)
     ## or use find and add mindepth and maxdepth
     # for f in * ;
     # do
+        # test -f "$n" || echo "$n is not a file."
+        # test -d "$n" || echo "$n is not a directory."
     # done
 
 ################################
