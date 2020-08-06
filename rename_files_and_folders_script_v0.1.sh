32fc76cafc39ff9f68169d885aa5268962302803
atkuzmanov <atkuzmanov@gmail.com>
2020-08-06 14:09:46 +0300

    Work-In-Progress-WIP-2020-08-06-140946

--- rename_files_and_folders_script_v0.1.sh
+++ rename_files_and_folders_script_v0.1.sh
@@ -19,28 +19,33 @@ STRINGS_TO_REPLACE=("alk8915" "alk198915")
 ## https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash
 
 rename_files_and_folders() {
-    while IFS=  read -r -d '' n; do
+    while IFS= read -r -d '' n; do
 
-        if [[ "$n" == *"alk19890105"* ]]; 
-        then
-            echo "Skipping $n"
-        else
-            for current_string in "${STRINGS_TO_REPLACE[@]}" ;
-            do
-                if [[ "$n" == *"$current_string"* ]]; 
-                then
-                    echo $n
-                fi
-            done
-        fi
+        # if [[ "$n" == *"alk19890105"* ]]; 
+        # then
+        #     echo "Skipping $n"
+        # else
+        #     for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+        #     do
+        #         if [[ "$n" == *"$current_string"* ]]; 
+        #         then
+        #             echo $n
+        #         fi
+        #     done
+        # fi
 
-        # echo "$n"
-    done < <(find . \( ! -regex '.*/\..*' \) -type f -print0)
+        echo "$n"
+    done < <(find . \( ! -regex '.*/\..*' -type d -o -type f \) -print0)
 }
 
 rename_files_and_folders
 
 ################################
+
+test_f1() {
+}
+
+# test_f1
 
 ################################
     
