12827a07b1c83c6bdfb121314c9e98536b88694c
atkuzmanov <atkuzmanov@gmail.com>
2020-08-07 12:31:23 +0300

    Work-In-Progress-WIP-2020-08-07-123123

--- rename_files_and_folders_script/rename_files_and_folders_script_v0.3.sh
+++ rename_files_and_folders_script/rename_files_and_folders_script_v0.3.sh
@@ -22,60 +22,38 @@ rename_files_remove_old_tags() {
         justpathnofile=${n%/*}
         # echo "$justpathnofile"
 
-
-        # if [[ -f "$filepathnodot" ]] && [[ ! -d "$filepathnodot" ]];
-        # then
-
-            for current_string in "${STRINGS_TO_REPLACE[@]}" ;
-            do
-                if [[ "$justfilenamenopath" == *"$current_string"* ]]; 
-                then
-                    # echo "Will rename $justfilenamenopath"
-                    test -e "$n" &&
-                        newfilename=$(echo "$justfilenamenopath" | sed "s/$current_string//g")
-                        mv -v "$n" "$justpathnofile/$newfilename"
-                    break;
-                fi
-            done
-
-        # fi
+        for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+        do
+            if [[ "$justfilenamenopath" == *"$current_string"* ]]; 
+            then
+                # echo "Will rename $justfilenamenopath"
+                test -e "$n" &&
+                    newfilename=$(echo "$justfilenamenopath" | sed "s/$current_string//g")
+                    mv -v "$n" "$justpathnofile/$newfilename"
+                break;
+            fi
+        done
     done < <(find . \( -type f -name "[!.]*" \) -print0)
 }
 
-# rename_files_remove_old_tags
+rename_files_remove_old_tags
 
 ################################
 
 rename_dirs_folders_remove_old_tags() {
     while IFS= read -r -d '' n; do
-
-        filepathnodot="${n#.}"
-        # echo "$filepathnodot"
-
-        # justfilenamenopath="${n##*/}"
-        # echo "$justfilenamenopath"
-
-        # justpathnofile=${n%/*}
-        # echo "$justpathnofile"
-
-
-        # if [[ -f "$filepathnodot" ]] && [[ ! -d "$filepathnodot" ]];
-        # then
-
-            for current_string in "${STRINGS_TO_REPLACE[@]}" ;
-            do
-                if [[ "$n" == *"$current_string"* ]]; 
-                then
-                    # echo "Will rename $n"
-                    test -e "$n" &&
-                        newfilename=$(echo "$n" | sed "s/$current_string//g")
-                        # echo "$newfilename"
-                        mv -v "$n" "$newfilename"
-                    break;
-                fi
-            done
-
-        # fi
+        for current_string in "${STRINGS_TO_REPLACE[@]}" ;
+        do
+            if [[ "$n" == *"$current_string"* ]]; 
+            then
+                # echo "Will rename $n"
+                test -e "$n" &&
+                    newfilename=$(echo "$n" | sed "s/$current_string//g")
+                    # echo "$newfilename"
+                    mv -v "$n" "$newfilename"
+                break;
+            fi
+        done
     done < <(find . \( -type d -name "[!.]*" \) -print0)
 }
 
@@ -102,15 +80,32 @@ rename_files_add_new_tags() {
                 newfilename="${justfilenamenopath%.*} $STRING_TO_ADD_IF_NOT_PRESENT.${justfilenamenopath##*.}"
                 mv -v "$n" "$justpathnofile/$newfilename"
         fi
-
     done < <(find . \( -type f -name "[!.]*" \) -print0)
 }
 
-# rename_files_add_new_tags
+rename_files_add_new_tags
 
 ################################
-################################
 
+rename_dirs_folders_add_new_tags() {
+    while IFS= read -r -d '' n; do
+        
+        filepathnodot="${n#.}"
+        # echo "$filepathnodot"
+
+        justpathnofile=${n%/*}
+        # echo "$justpathnofile"
+
+        if [[ ! "$n" == *"$STRING_TO_ADD_IF_NOT_PRESENT"* ]]; 
+        then
+            test -e "$n" &&
+                newfilename="$n $STRING_TO_ADD_IF_NOT_PRESENT"
+                mv -v "$n" "$newfilename"
+        fi
+    done < <(find . \( -type d -name "[!.]*" \) -print0)
+}
+
+rename_dirs_folders_add_new_tags
 
 ################################
 ################################
