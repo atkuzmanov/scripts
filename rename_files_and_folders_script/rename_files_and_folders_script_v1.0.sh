6d6360906c521c4956e699804126e52a5847ce99
atkuzmanov <atkuzmanov@gmail.com>
2020-08-07 13:24:11 +0300

    Work-In-Progress-WIP-2020-08-07-132411

--- rename_files_and_folders_script/rename_files_and_folders_script_v1.0.sh
+++ rename_files_and_folders_script/rename_files_and_folders_script_v1.0.sh
@@ -22,7 +22,21 @@ set -e
 ## witch path to follow.
 ##
 ## References
-## 
+## https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+## https://stackoverflow.com/questions/15012631/rename-files-and-directories-recursively-under-ubuntu-bash
+## https://superuser.com/questions/213134/recursively-rename-files-change-extension-in-linux
+## https://stackoverflow.com/questions/6509650/extract-directory-from-path
+## https://stackoverflow.com/questions/6121091/get-file-directory-path-from-file-path/6121114
+## https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
+## https://tldp.org/LDP/abs/html/string-manipulation.html
+## https://stackoverflow.com/questions/16623835/remove-a-fixed-prefix-suffix-from-a-string-in-bash
+## https://unix.stackexchange.com/questions/311758/remove-specific-word-in-variable
+## https://unix.stackexchange.com/questions/56810/adding-text-to-filename-before-extension
+## https://stackoverflow.com/questions/45799657/bash-adding-a-string-to-file-name
+## https://linuxize.com/post/bash-functions/
+## https://bash.cyberciti.biz/guide/Pass_arguments_into_a_function
+## https://stackoverflow.com/questions/6212219/passing-parameters-to-a-bash-function
+## https://linuxacademy.com/blog/linux/conditions-in-bash-scripting-if-statements/
 ################################
 
 declare -a STRINGS_TO_REPLACE
